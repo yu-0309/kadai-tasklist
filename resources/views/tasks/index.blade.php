e15a69e41deaeeb35e7c583466d8eb160fcee0ad
yu.kaku <gkaku0242@gmail.com>
2020-03-31 09:49:55 +0000

    init tasklists Project

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+@endsection
